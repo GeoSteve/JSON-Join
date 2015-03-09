702dd6dfc5fb5a01cbd457afb147a0a48bb7561a
Steven Heinrich <steven.heinrich@gmail.com>
2015-03-09 07:35:35 -0400

    update jsonJoin function

--- JSON-Join.js
+++ JSON-Join.js
@@ -1,29 +1,38 @@
-var family1 = [
-    {id: 1, firstName: 'Steve', interests: ['beer']},
-    {id: 5, firstName: 'Tucker'},
-    {id: 3, firstName: 'Jackson'}
+var DraftRound1 = [
+    {id: 1, firstName: 'Steven', interests: ['beer']},
+    {id: 5, firstName: 'Tucker', lastName: 'Max', interests: ['debauchery']},
+    {id: 3, firstName: 'Jack'}
 ];
 
-var family2 = [
-    {id: 1, firstName: 'Steve', lastName: 'Heinrich'},
-    {id: 2, firstName: 'Erin', lastName: 'Heinrich'},
-    {id: 3, lastName: 'Heinrich'},
-    {id: 4, firstName: 'Griffin', lastName: 'Heinrich'}
+var DraftRound2 = [
+    {id: 1, firstName: 'Steven', lastName: 'Heinrich'},
+    {id: 2, firstName: 'Aaron', lastName: 'Henry'},
+    {id: 3, lastName: 'Griffin'},
+    {id: 4, firstName: 'Hank', lastName: 'Berman'}
 ];
 
 
+function jsonJoin(input1, input2, joinKey) {
+    var joinResult = [];
 
-// JSON Object Join on ID, SQL Join Style (with LowDash)
-var joinResult = [];
-_.forEach(family1, function(currentUser) {
-    // Join common objects
-    var joinUser = _.find(family2, {'id': currentUser.id});
-    joinResult.push(_.assign(currentUser, joinUser));
-});
-// Merge in the remaining objects from 2nd array
-joinResult = joinResult.concat(family2);
+    _.forEach(input1, function(currentUser) {
+        // Join common objects
+        var joinOptions = {}
+        joinOptions[joinKey] = currentUser.id
+        var joinUser = _.find(input2, joinOptions);
+        joinResult.push(_.assign(currentUser, joinUser));
+    });
 
-// Limit to unique objects
-joinResult = _.uniq(joinResult, 'id');
-// Reoder the array of objects
-joinResult = _.sortBy(joinResult, 'id');
+    // Merge in the remaining objects from 2nd array
+    joinResult = joinResult.concat(input2);
+
+    // Limit to unique objects
+    joinResult = _.uniq(joinResult, 'id');
+
+    // Reoder the array of objects
+    joinResult = _.sortBy(joinResult, 'id');
+
+    return joinResult;
+}
+
+myJoin = jsonJoin(DraftRound1, DraftRound2, 'id');
